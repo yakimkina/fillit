e0c6f2c4d109364832a85787819760b1345df141
Blaquesmith Nigellus <bnigellu@il-b4.21-school.ru>
2019-04-24 19:13:49 +0300

    ZANUDA

---
+++ fillit.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnigellu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/24 14:06:16 by bnigellu          #+#    #+#             */
+/*   Updated: 2019/04/24 17:34:18 by bnigellu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <libc.h>
+# include "libft/includes/libft.h"
+# include <stdio.h>
+
+#endif
