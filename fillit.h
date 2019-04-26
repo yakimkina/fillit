d3198ad66569c02d42f79a5a803c0ae4ecbc6689
Blaquesmith Nigellus <bnigellu@il-a2.21-school.ru>
2019-04-26 19:08:39 +0300

    from LIZA

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: bnigellu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 13:08:59 by bnigellu          #+#    #+#             */
-/*   Updated: 2019/04/26 16:27:07 by enikole          ###   ########.fr       */
+/*   Updated: 2019/04/26 18:52:29 by bnigellu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@
 # include "libft/includes/libft.h"
 # include <stdio.h>
 
-t_list	*end_of_lists(t_list **alst);
+int		lesen(int fd, t_list **list);
+
+void	list_free(t_list **alst);
+t_list	**lst_end_add(t_list **alst, int alfa);
+
 void	alg(int amt, t_list **list);
+void	make_map(char ***map, int sz);
 int		try_map(int sz, char **map, t_list *list);
 void	del_map(char **map, t_list *list, int i, int j);
 void	fill_map(char **map, t_list *list, int i, int j);
